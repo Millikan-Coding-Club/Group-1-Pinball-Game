5ebee2df1330b0a461c2081be3a0f8aca7d80a2b
YeetmyFeet <yeetmyfeet69420bruh@gmail.com>
2023-10-05 19:29:40 -0700

    Version 0.3
    -updated README
    -added score placeholder
    -added flipper animations
    -updated game controller to listen for input and move flippers
    -added (scuffed) physics to flippers, boundaries, and the ball
    -changed boundary color

--- Pinball/Assets/GameController.cs
+++ Pinball/Assets/GameController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
-{
+{   
+    public Animator leftPaddleAnimator;
+    public Animator rightPaddleAnimator;
 
     // Update is called once per frame
     void Update()
     {
         if ( Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) ) {
-
+            leftPaddleAnimator.ResetTrigger("Pressed");
+            leftPaddleAnimator.SetTrigger("Pressed");
         }
 
         if ( Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) ) {
-            
+            rightPaddleAnimator.ResetTrigger("Pressed");
+            rightPaddleAnimator.SetTrigger("Pressed");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Group-1-Pinball-Game
 
-Sample game made by Chris, Dara, and Leo using [this tutorial](https://www.youtube.com/watch?v=3ZUPA9QEVoU)
+Sample game made by Chris, Dara, Leo, and Jacob using [this tutorial](https://www.youtube.com/watch?v=3ZUPA9QEVoU)
